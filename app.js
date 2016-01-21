677ee8644a1d40507911f9ecd4e85188ee8e00df
bahadur <bahadurfarooq89@gmail.com>
2016-01-21 22:39:39 +0500

    Create App.js

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Bahadur on 21-Jan-16.
+ */
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000);
+console.log('Server running at http://localhost:3000/');
